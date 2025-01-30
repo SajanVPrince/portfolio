28f557f2365e4b2941e9c972aa7fc28094b99f52
SajanVPrince <sajanvprince@gmail.com>
2025-01-30 09:59:39 +0530

    resume added

--- portfolio/main.py
+++ portfolio/main.py
@@ -1,12 +1,5 @@
 from flask import Flask,render_template,request
-import sqlite3
-con=sqlite3.connect('D:/Sajan2/portfolio/portfolio/data.db')
-try:
-    con.execute("create table details(name text,email text,phone text,subject text,message text)")
-except:
-    pass
 
-# 
 # set FLASK_APP=main.py
 
 app=Flask(__name__)
